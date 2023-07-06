6d6dce0e30a625868704ec095bda4ab052c70aa0
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-07-06 00:18:17 -0700

    Backport argument stuff from mineman-sdl-ui branch

---
+++ src/args.c
@@ -0,0 +1,41 @@
+#include <mtocptwm.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+extern int GUI_Main();
+extern bool mode;
+void handleArgs(int argc, char *argv[]) {
+	bool badArgs = false;
+	if (argc > 2)  {badArgs = true;}
+	if (argc == 2) {
+		if (argv[1][0] == 'r') {mode = true;}
+		else if (argv[1][0] == 't') {mode = false;}
+		else {badArgs = true;}
+	}
+	if (badArgs) {
+		puts("Please give argument `r` or `t` for recieve or transmit, or none to start GUI.");
+		platformExit(true);
+	}
+	// no args, handle GUI stuff
+	if (argc == 1) {
+		pid_t pid = fork();
+		if (pid == -1) {
+			// error
+			perror("fork");
+			exit(1);
+		}
+		if (pid == 0) {
+			// TODO: child process, find some way to communicate with the parent, then wait for the parent (SDL) to tell us the mode.  Some ideas, ranked in order of how good they are:
+			// - redirect stdout to the parent's stdin (would require minimal changes, like opening a pipe, then redirecting our stdout to the pipe's input, and redirecting our stdin to the pipe's output)
+			// - IPC
+			// - temporary files
+			while (true) { sleep(5); }
+		}
+		if (pid >= 1) {
+			// start GUI here
+			exit(0);
+		}
+	}
+}
--- src/main.c
+++ src/main.c
@@ -10,27 +10,14 @@
 extern void setupNet();
 extern void broadcastLoop(const char *message, const bool mode);
 extern void platformInit(int *argc, char *argv[]);
+extern void handleArgs(int argc, char *argv[]);
+bool mode;
 #include <mtocptwm.h>
 
 int main(int argc, char *argv[]) {
 	platformInit(&argc, argv);
-	bool badArgs = false;
-	bool mode;
-	if (argc != 2) {badArgs = true;}
-	if (argv[1][0] == 'r') {mode = true;}
-	else if (argv[1][0] == 't') {mode = false;}
-	else {badArgs = true;}
-	if (badArgs) {
-		puts("Please give argument `r` or `t` for recieve or transmit.");
-		platformExit(true);
-	}
-	#if __SWITCH__
-		// it seems like if we don't call this every once in a while, everything just dies
-		// just in case we're approaching that time once we get here, 
-		if (!appletMainLoop()) {
-			exit(0);
-		}
-	#endif
+	setupNet();
+	handleArgs(argc, argv);
 	setupNet();
 
 	char message[124];
