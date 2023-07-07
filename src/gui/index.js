41287c14f7ea8279f6ce4f48c0af9d95cba7cc81
mine-man3000 <gdev0580@gmail.com>
2023-07-06 21:54:32 -0700

    fixed some things

--- src/gui/index.js
+++ src/gui/index.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const fs = require('fs')
 
 try {
 	//require('electron-reloader')(module)
--- src/gui/yourmom.js
+++ src/gui/yourmom.js
@@ -1,20 +1,18 @@
-function loadInterface() {
+const fs = require('fs')
 
-	const path = './mtocptwm.cfg'
+const path = './mtocptwm.cfg'
 
-	try {
-		if (fs.existsSync(path)) {
-			test = document.createElement("h1")
-			test.innerText = "YES CONFIG"
-			document.appendChild(test)
-		}
-		else {
-			test = document.createElement("h1")
-			test.innerText = "NO CONFIG"
-			document.appendChild(test)
-		}
-	} catch (err) {
-		console.error(err)
+try {
+	if (fs.existsSync(path)) {
+		test = document.createElement("h1")
+		test.innerText = "YES CONFIG"
+		document.appendChild(test)
 	}
+	else {
+		test = document.createElement("h1")
+		test.innerText = "NO CONFIG"
+		document.appendChild(test)
+	}
+} catch (err) {
+	console.error(err)
 }
-
